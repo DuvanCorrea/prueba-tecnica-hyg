f10cedc6bb4ef08f98e4d939e9bd0d211150d238
DuvanCorrea <duvancorrea96@gmail.com>
2021-06-25 10:25:51 -0500

    changes

--- src/controllers/seguimientoCrtl.js
+++ src/controllers/seguimientoCrtl.js
@@ -1,7 +1,8 @@
 const client = require("../configs/database");
 
 // QUERIS
-const GET = "SELECT * FROM SEGUIMINETOS WHERE (PROYECTO_codigo_proyecto=$1)" 
+const GET = "SELECT * FROM SEGUIMINETOS WHERE (PROYECTO_codigo_proyecto=$1)"
+const POST = "INSERT INTO SEGUIMINETOS (fecha,descripccion,avance,PROYECTO_codigo_proyecto) VALUES ($1,$2,$3,$4)"
 
 const seguimientoCtrl = {
     get: (req, res) => {
@@ -31,6 +32,29 @@ const seguimientoCtrl = {
             } catch (error) {
                 res.status(500)
                 console.log("ERROR CONSULTA BD >>>", error)
+                res.send(error)
+            }
+        })
+
+    },
+
+    post: (req, res) => {
+        console.log(req.body)
+
+        const { fecha, descripccion, avance, PROYECTO_codigo_proyecto } = req.body
+
+        client.connect()
+        client.query(POST, [fecha, descripccion, avance, PROYECTO_codigo_proyecto], (err, response) => {
+            try {
+                if (err) {
+                    console.log("ERROR CONSULTA BD >>>", err)
+                    res.send(err)
+                } else {
+                    res.send({ mensaje: "seguimiento guardado" })
+                }
+            } catch (error) {
+                console.log("ERROR CONSULTA BD >>>", err)
+                res.status(500)
                 res.send(error)
             }
         })
--- src/routes/seguimiento.routes.js
+++ src/routes/seguimiento.routes.js
@@ -3,5 +3,6 @@ const seguimientoCtrl = require("../controllers/seguimientoCrtl")
 const router = express.Router()
 
 router.get("/seguimientos/:codigo_proyecto", seguimientoCtrl.get)
+router.post("/seguimiento", seguimientoCtrl.post)
 
 module.exports = router;
