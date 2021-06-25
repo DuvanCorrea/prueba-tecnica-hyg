d1f6b3af76ffdf2d4d800384de125ddb9f2d008c
DuvanCorrea <duvancorrea96@gmail.com>
2021-06-25 11:43:37 -0500

    listo?

--- src/controllers/proyectoCtrl.js
+++ src/controllers/proyectoCtrl.js
@@ -10,6 +10,10 @@ const POSTWITHPROYECTO = "SELECT P.codigo_proyecto, P.fecha_inicio, P.fecha_fin,
 const POSTWITHLIDER = "SELECT P.codigo_proyecto, P.fecha_inicio, P.fecha_fin, P.objetivo, P.descripccion, P.direccion, P.costo_total, P.longitud, P.latitud, L.codigo_lider, L.nombre AS nombre_lider, E.codigo_estado, E.nombre AS nombre_estado, EN.nit, EN.nombre AS nombre_entidad FROM PROYECTOS P, LIDERES L, ESTADOS E, ENTIDADES EN WHERE (P.LIDER_codigo_lider=$1 AND P.LIDER_codigo_lider=L.codigo_lider AND P.ESTADOS_codigo_estado=E.codigo_estado AND P.ENTIDAD_nit=EN.nit)"
 const POSTWITHFECHA = "SELECT P.codigo_proyecto, P.fecha_inicio, P.fecha_fin, P.objetivo, P.descripccion, P.direccion, P.costo_total, P.longitud, P.latitud, L.codigo_lider, L.nombre AS nombre_lider, E.codigo_estado, E.nombre AS nombre_estado, EN.nit, EN.nombre AS nombre_entidad FROM PROYECTOS P, LIDERES L, ESTADOS E, ENTIDADES EN WHERE (P.fecha_inicio=$1 AND P.LIDER_codigo_lider=L.codigo_lider AND P.ESTADOS_codigo_estado=E.codigo_estado AND P.ENTIDAD_nit=EN.nit)"
 
+
+let proyectosDevolver = []
+let contador;
+
 const proyectoCtrl = {
     get: (req, res) => {
         console.log("holi")
@@ -87,7 +91,42 @@ const proyectoCtrl = {
                         res.status(404)
                         res.send({ mensaje: "No se encontro el proyecto" })
                     } else {
-                        res.send(response.rows)
+                        proyectosDevolver = response.rows
+                        contador = proyectosDevolver.length
+
+                        // hacer consulta pasa saber que cantidad de seguimientos tiene cada proyecto
+                        proyectosDevolver.forEach(e => {
+
+
+                            client.query("SELECT count(*) FROM SEGUIMINETOS WHERE (PROYECTO_codigo_proyecto=$1)", [e.codigo_proyecto], (err, response) => {
+                                try {
+
+                                    if (err) {
+                                        console.log("ERROR CONSULTA BD >>>", err)
+                                        res.send(err)
+                                    } else {
+                                        // console.log("Aqui", response.rows)
+                                        e.count = response.rows[0].count
+
+                                        contador -= 1
+                                        // aqui finalmente se responde al cliente
+                                        if (contador == 0) {
+                                            res.send(proyectosDevolver)
+                                        }
+                                    }
+                                } catch (error) {
+                                    console.log("ERROR CONSULTA BD >>>", err)
+                                    res.status(500)
+                                    res.send(error)
+                                }
+                            })
+
+
+                        })
+
+                        // res.send(proyectosDevolver)
+
+                        // res.send(response.rows)
                     }
                 }
             } catch (error) {
--- src/controllers/seguimientoCrtl.js
+++ src/controllers/seguimientoCrtl.js
@@ -4,6 +4,9 @@ const client = require("../configs/database");
 const GET = "SELECT * FROM SEGUIMINETOS WHERE (PROYECTO_codigo_proyecto=$1)"
 const POST = "INSERT INTO SEGUIMINETOS (fecha,descripccion,avance,PROYECTO_codigo_proyecto) VALUES ($1,$2,$3,$4)"
 
+// Actualizar estado del proyecto
+const UPDATE = "UPDATE PROYECTOS SET estados_codigo_estado=$1 WHERE (codigo_proyecto=$2);"
+
 const seguimientoCtrl = {
     get: (req, res) => {
 
@@ -41,7 +44,8 @@ const seguimientoCtrl = {
     post: (req, res) => {
         console.log(req.body)
 
-        const { fecha, descripccion, avance, PROYECTO_codigo_proyecto } = req.body
+        const { fecha, descripccion, avance, PROYECTO_codigo_proyecto, estadoProyecto } = req.body
+
 
         client.connect()
         client.query(POST, [fecha, descripccion, avance, PROYECTO_codigo_proyecto], (err, response) => {
@@ -50,7 +54,20 @@ const seguimientoCtrl = {
                     console.log("ERROR CONSULTA BD >>>", err)
                     res.send(err)
                 } else {
-                    res.send({ mensaje: "seguimiento guardado" })
+                    client.query(UPDATE, [estadoProyecto, PROYECTO_codigo_proyecto], (err, response) => {
+                        try {
+                            if (err) {
+                                console.log("ERROR CONSULTA BD >>>", err)
+                                res.send(err)
+                            } else {
+                                res.send({ mensaje: "seguimiento guardado y estado actualizado" })
+                            }
+                        } catch (error) {
+                            console.log("ERROR CONSULTA BD >>>", err)
+                            res.status(500)
+                            res.send(error)
+                        }
+                    })
                 }
             } catch (error) {
                 console.log("ERROR CONSULTA BD >>>", err)
